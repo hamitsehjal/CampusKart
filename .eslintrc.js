d5f72b3c9301bd639d828c870058593c22164177
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-09-17 16:59:39 -0400

    Configure eslint and .gitignore for the project

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "jest": true,
+    },
+    "extends": ["eslint:recommended", "prettier"],
+    "parserOptions": {
+        "ecmaVersion": latest
+    },
+    "rules": {
+
+    }
+}
