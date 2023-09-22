f3e5c61d4ecc042b2f5d0623186815627fbb67e9
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-09-22 00:28:32 -0400

    wrote tests for Password(required and being hashed)

--- tests/unit/schemas/user.test.js
+++ tests/unit/schemas/user.test.js
@@ -1,4 +1,5 @@
 const { dbConnect, dbDisconnect } = require('../test_connection');
+const bcrypt = require('bcrypt');
 const User = require('../../../src/models/user')
 beforeAll(async () => {
     await dbConnect();
@@ -68,3 +69,44 @@ describe('Validation and logic works for Email Address', () => {
         }
     })
 })
+
+describe('Password: Validation and Business Logic', () => {
+    test('Password is required', async () => {
+        try {
+            await User.create({
+                emailAddress: "alexlin3@myseneca.ca",
+                studentId: 111222333,
+            })
+
+            throw new Error('Create operation should not succeed')
+
+        } catch (err) {
+            // expect(err.errors['password'].message).toEqual("Path `password` is invalid (1231231)")
+            expect(err.message).toContain('password')
+            expect(err.message).toContain('required')
+        }
+
+    });
+
+    test('Password should be hashed', async () => {
+        const plainPassword = "testing";
+        const user = new User({
+            emailAddress: "test@myseneca.ca",
+            password: plainPassword,
+            studentId: 111000111,
+        })
+
+        expect(user.password).toEqual(plainPassword);
+
+        // Saving the user (triggers pre('save') hook for hashing)
+        await user.save();
+
+        // Check if the new user's password is hashed and equal to pl
+        const retrievedUser = await User.findOne({ emailAddress: "test@myseneca.ca" });
+
+        expect(retrievedUser).toBeDefined();
+        expect(retrievedUser.password).not.toEqual(plainPassword);
+        expect(await bcrypt.compare(plainPassword, retrievedUser.password)).toBe(true);
+    })
+})
+
