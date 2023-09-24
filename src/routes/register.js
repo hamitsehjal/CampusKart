08eff10e563a1e0d995453570d41623172b37e48
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-09-24 19:33:47 -0400

    refactor console.log statemens with structured logging(pino)

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,44 +1,46 @@
 // src/routes/api/register.js 
 const { createErrorResponse, createSuccessResponse } = require('../response');
 const { User } = require('../models');
+const logger = require('../logger');
 module.exports = async (req, res) => {
-    /**
-     * 1. receive information as 'req.body'
-     * 2. calls registerUser with req.body
-  
-     */
-    try {
-        await registerUser(req.body);
-        const successResponse = createSuccessResponse({
-            message: "Account Created!!"
-        })
-        res.status(201).json(successResponse);
-    } catch (err) {
+  /**
+   * 1. receive information as 'req.body'
+   * 2. calls registerUser with req.body
+ 
+   */
+  try {
+    await registerUser(req.body);
+    const successResponse = createSuccessResponse({
+      message: "Account Created!!"
+    })
+    res.status(201).json(successResponse);
+  } catch (err) {
 
-        return res.status(422).json(createErrorResponse(422, "Couldn't create Account!!"));
-    }
+    return res.status(422).json(createErrorResponse(422, "Couldn't create Account!!"));
+  }
 
 }
 
 
 async function registerUser(data) {
-    /**
-     * 1. Make a new Mongoose document and save it up in the database
-     * 2. Let user know the response    
-     *  - If saved - success message
-     *  - If not saved - error message
-     */
-    try {
-        await User.create({
-            emailAddress: data.emailAddress,
-            password: data.password,
-            studentId: data.studentId,
-            profilePicture: data.profilePicture ?? '',
-        });
+  /**
+   * 1. Make a new Mongoose document and save it up in the database
+   * 2. Let user know the response    
+   *  - If saved - success message
+   *  - If not saved - error message
+   */
+  try {
+    await User.create({
+      emailAddress: data.emailAddress,
+      password: data.password,
+      studentId: data.studentId,
+      profilePicture: data.profilePicture ?? '',
+    });
+    logger.info(`SUCCESS: User ${data.emailAddress} has been created`)
+    return (`SUCCESS: User ${data.emailAddress} has been created`);
 
-        return (`SUCCESS: User ${data.emailAddress} has been created`);
-
-    } catch (err) {
-        throw new Error(`FAILED: User ${data.emailAddress} WAS NOT created - ${err}`);
-    }
+  } catch (err) {
+    logger.err({ err }, `FAILED: User ${data.emailAddress} WAS NOT created`);
+    throw new Error(`FAILED: User ${data.emailAddress} WAS NOT created - ${err}`);
+  }
 }
