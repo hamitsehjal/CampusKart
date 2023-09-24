740caa24ae30f8eb028969f05a10668c6c94d127
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-09-24 19:21:48 -0400

    Set up middleware for the express app

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
 const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+const compression = require('compression');
 const { createErrorResponse } = require('./response');
-
+const logger = require('./logger');
+const pino = require('pino-http')(
+  {
+    // Use our default logger, that is already configured
+    logger,
+  }
+)
 
 const app = express();
 
+// Use pino for structured logging
+app.use(pino);
+
+// security middleware
+app.use(helmet());
+
 // middleware to parse json requests
 app.use(express.json());
+
+// enables cross-site resource sharing
+app.use(cors())
+
+// compress the response headers
+
+app.use(compression());
 // Define routes
 
 
@@ -16,23 +38,23 @@ app.use('/', require('./routes'));
 
 // Catch-All Routes for unmatched URLs 
 app.use((req, res) => {
-    const errorResponse = createErrorResponse(404, "Resource doesn't exist!!")
-    res.status(404).json(errorResponse);
+  const errorResponse = createErrorResponse(404, "Resource doesn't exist!!")
+  res.status(404).json(errorResponse);
 })
 
 // Error Handling Middleware
 app.use((err, req, res) => {
-    const errCode = err.statusCode || 500;
-    const errMsg = err.statusMessage || 'Unable to process Request';
+  const errCode = err.statusCode || 500;
+  const errMsg = err.statusMessage || 'Unable to process Request';
 
-    // If there is any server error, we want to see what's happening
-    if (errCode > 499) {
-        console.log(err);
-    }
+  // If there is any server error, we want to see what's happening
+  if (errCode > 499) {
+    logger.error({ err }, `Error processing request`);
+  }
 
-    const errorResponse = createErrorResponse(errCode, errMsg);
+  const errorResponse = createErrorResponse(errCode, errMsg);
 
-    res.status(errCode).json(errorResponse);
+  res.status(errCode).json(errorResponse);
 
 })
 module.exports = app;
