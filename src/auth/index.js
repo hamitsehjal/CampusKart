7c40a801c2c94f5d15db053ae36f52a51a22f69d
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-17 11:40:41 -0400

    Fix empty Bearer token error

--- src/auth/index.js
+++ src/auth/index.js
@@ -8,7 +8,9 @@ module.exports = (req, res, next) => {
    * 3. If verified, attach the payload to the request object
    */
   // const token = req.headers.authorization.split(" ")[1];
-  const token = req.headers.authorization.split(' ')[1];
+  const authorizationHeader = req.headers.authorization;
+  const token = authorizationHeader && authorizationHeader.trim() !== '' ? authorizationHeader.split(' ')[1] : ""
+  // const token = req.headers.authorization.split(' ')[1] || "";
   try {
     // Verify the token
     const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
