e83bb3a12395c6136af3c2bbd4edb1a73a8a0379
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-17 11:31:37 -0400

    set up the name of the database

--- src/config/database.js
+++ src/config/database.js
@@ -4,6 +4,7 @@ const logger = require('../logger');
 exports.connectToDatabase = () => {
   const URI = process.env.MONGODB_URI;
   mongoose.connect(URI, {
+    dbName: "campus-cart-app",
     useNewUrlParser: true, // use new URL parser of MongoDB Driver
     useUnifiedTopology: true, // use new server Discovery and Monitoring Engine
   }).then(() => {
