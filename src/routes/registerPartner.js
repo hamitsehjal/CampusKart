6821648255ca6168a075d40a3193301038bf3967
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-17 23:01:25 -0400

    parnter registration API Endpoint up and running✔️

--- src/routes/registerPartner.js
+++ src/routes/registerPartner.js
@@ -7,6 +7,26 @@ module.exports = async (req, res) => {
    * 1. Extract information from the req.body
    * 2. Save it up in the Database
    */
+  /**
+   * Testing Purposes
+   * {
+  "storeName":"Walmart",
+  "storeEmail":"bayview@walmart.ca",
+  "storeAddress":{
+    "postalCode":"L7A 2V5",
+    "addressLine1":"131 Jolne Crescent",
+    "addressLine2":"",
+    "city":"Toronto",
+    "state":"Ontario",
+    "country":"Canada"
+  },
+  "storeDescription":"We offer wide-range groceries and fruit items",
+  "contactFirstName":"Allen",
+  "contactLastName":"Burg"
+}
+
+   * 
+   */
   try {
     const { storeName, storeEmail, storeAddress, storeDescription, contactFirstName, contactLastName } = req.body;
 
