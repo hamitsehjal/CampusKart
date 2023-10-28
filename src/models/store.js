c195009037311294b48940b6eee6d5fbe2ba8819
Hamit Sehjal <hamitsehjal26@gmail.com>
2023-10-28 00:06:17 -0400

    Refactor stores model - add category and thumbnailImage properties

--- src/models/store.js
+++ src/models/store.js
@@ -3,50 +3,70 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const addressSchema = new Schema({
-    postalCode: {
-        type: String,
-        required: true,
-    },
-    addressLine1: {
-        type: String,
-        required: true,
-    },
-    addressLine2: {
-        type: String,
-        default: "",
-    },
-    city: {
-        type: String,
-        required: true,
-    },
-    state: {
-        type: String,
-        required: true,
-    },
-    country:
-    {
-        type: String,
-        required: true,
-    }
+  postalCode: {
+    type: String,
+    required: true,
+  },
+  addressLine1: {
+    type: String,
+    required: true,
+  },
+  addressLine2: {
+    type: String,
+    default: "",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  state: {
+    type: String,
+    required: true,
+  },
+  country:
+  {
+    type: String,
+    required: true,
+  }
 
 })
 const storeSchema = new Schema({
-    name: {
-        type: String,
-        required: true
+  name: {
+    type: String,
+    required: true
+  },
+  //Grocery, alcohol, wholesale, convenience,pets, beauty,electronics,sports gear
+  category: {
+    type: [String],
+    enum: {
+      values: [
+        'Grocery',
+        'Pets',
+        'Electronics',
+        'Beauty',
+        'Wholesale',
+        'Convenience',
+        'Alcohol',
+        'Sports Gear',
+      ],
+      message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
     },
-    description: {
-        type: String,
-        required: true
-    },
-    location: {
-        type: addressSchema,
-        required: true,
-    },
-    openHours: {
-        type: String,
-        required: true,
-    }
+    required: true,
+    index: true,
+  },
+  thumbnailImage: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+  },
+  location: {
+    type: addressSchema,
+  },
+  openHours: {
+    type: String,
+  }
 })
 
 const Store = mongoose.model('Store', storeSchema);
