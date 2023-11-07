5b0941495e3d4926296f8c5f719ea6b6c60de59a
Replit user <>
2023-11-07 06:41:43 +0000

    Refactor Products Model

--- src/models/product.js
+++ src/models/product.js
@@ -5,76 +5,74 @@ const User = require('./user');
 const { Schema } = mongoose;
 
 const reviewSchema = new Schema({
-    userId: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: User,
-        required: true,
-    },
-    rating: {
-        type: Number,
-        required: true,
-        min: [1, 'Must be at least 1, got {VALUE}'],
-        max: [5, 'can be at most 5, got {VALUE}'],
-    },
-    comment: String,
+  userId: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: User,
+    required: true,
+  },
+  rating: {
+    type: Number,
+    required: true,
+    min: [1, 'Must be at least 1, got {VALUE}'],
+    max: [5, 'can be at most 5, got {VALUE}'],
+  },
+  comment: String,
 }, { timestamps: true })
 
 const productSchema = new Schema({
-    storeId: {
-        type: mongoose.SchemaTypes.ObjectId,
-        ref: Store,
-        required: true,
+  name: {
+    type: String,
+    required: true,
+  },
+  storeId: {
+    type: mongoose.SchemaTypes.ObjectId,
+    ref: Store,
+    required: true,
+  },
+  description: String,
+  price: {
+    type: Number,
+    required: true,
+    min: [0, 'Must be a positive value, got {VALUE}'] //should be a positive value
+  },
+  category: {
+    type: String,
+    enum:
+    {
+      values: [
+        'Fruits',
+        'Vegetables',
+        'Dairy',
+        'Meat',
+        'Seafood',
+        'Bakery',
+        'Canned Goods',
+        'Frozen Foods',
+        'Snacks',
+        'Beverages',
+        'Household',
+        'Personal Care',
+        'Cleaning Supplies',
+        'Baby Care',
+      ],
+      message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
     },
-    name: {
-        type: String,
-        required: true,
-    },
-    description: String,
-    price: {
-        type: Number,
-        required: true,
-        min: [0, 'Must be a positive value, got {VALUE}'] //should be a positive value
-    },
-    stockQuantity: {
-        type: Number,
-        required: true,
-        min: [0, 'Must be a positive value, got {VALUE}']// should be a positive value
-    },
-    image: {
-        type: Buffer,
-        default: Buffer.from(''),
-    },
-    category: {
-        type: String,
-        enum:
-        {
-            values: [
-                'Fruits',
-                'Vegetables',
-                'Dairy',
-                'Meat',
-                'Seafood',
-                'Bakery',
-                'Canned Goods',
-                'Frozen Foods',
-                'Snacks',
-                'Beverages',
-                'Household',
-                'Personal Care',
-                'Cleaning Supplies',
-                'Baby Care',
-            ],
-            message: 'enum validator failed for path `{PATH}` with value `{VALUE}`'
-        },
-        required: true,
-        index: true,
-    },
-    reviews: {
-        type: [reviewSchema],
-        default: []
-    },
+    required: true,
+    index: true,
+  },
+  imageName: {
+    type: String,
+    default: "products/default_product",
+  },
+  imageUrl: {
+    type: String,
+  },
+  reviews: {
+    type: [reviewSchema],
+    default: []
+  },
 }, {
-    timestamps: true
+  timestamps: true
 });
 
 const Product = mongoose.model('Product', productSchema);
