dd1d989bb14110c00f432f71ac73c1d90d0072a3
Replit user <>
2023-11-07 06:51:54 +0000

    Added support for requests specifying product category besides storeId

--- src/routes/private/products.js
+++ src/routes/private/products.js
@@ -1,6 +1,7 @@
 const logger = require("../../logger");
-const { Store } = require("../../models");
+const { Product } = require("../../models");
 const { createErrorResponse, createSuccessResponse } = require("../../response");
+const { generateS3ImageUrl } = require('../../config/s3Client');
 
 /**
  * Returns products for a specific stores based on storeId received as route parameter
@@ -8,24 +9,48 @@ const { createErrorResponse, createSuccessResponse } = require("../../response")
  */
 module.exports = async (req, res) => {
   /**
-   * 1. Extract id from the req object 
-   * 2. Retrieve a list of products from products model based on storeId
+   - Extract id from the req object 
+   - Extract 'category' from query parameters
+   - Retrieve a list of products from products model based on storeId
+      - if category doesn't exists, fetch all products
+      - else fetch products of specific category
+   - If no products found, return 200 with appropriate message
+   - else, generate pre-signed url for each product image
+   - return 200 with products object
    */
   const id = req.params.storeId;
+  const category = req.query.category || "all";
+  let products = []
   try {
-    const products = await Store.find({ storeId: id });
-
-    if (products.length == 0) {
-      logger.info(`No Products found for storeId: ${id}`);
-      return res.status(404).json(createErrorResponse(404, `No Products found for storeId: ${id}`));
+    if (category == "all") {
+      products = await Product.find({ storeId: id });
     }
     else {
-      logger.info(`Products retrieved for storedId: ${id}`);
-      return res.status(200).json(createSuccessResponse({ data: products }));
+      products = await Product.find({
+        storeId: id,
+        category: category,
+      });
+    }
+
+    if (products.length == 0) {
+      logger.info(`No Products found for storeId: ${id} with category: ${category}`);
+      return res.status(200).json(createErrorResponse(200, `No Products found for storeId: ${id} with category: ${category}`));
+    }
+    else {
+      logger.info(`Products retrieved for storedId: ${id} with category: ${category}`);
+      logger.debug({ data: products });
+
+      // Create pre-signed URLs for product images
+      for (const product of products) {
+        const url = await generateS3ImageUrl(product.imageName);
+        product.imageUrl = url;
+      }
+      logger.debug({ data: products });
+      return res.status(200).json(createSuccessResponse({ products: products }));
     }
 
   } catch (err) {
-    logger.error({ err }, `Error retrieving products for storeId: ${id}`);
+    logger.error({ err }, `Error retrieving products for storeId: ${id} and category: ${category}`);
     return res.status(500).json(createErrorResponse(500, `Internal Server Error`));
   }
 
