995c9760dc3b3ee90a701d9a73f7d4f936ab1a13
Replit user <>
2023-11-07 07:36:06 +0000

    Add route handler for product-categories URL

--- src/routes/private/index.js
+++ src/routes/private/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 
-
+// products
 router.get('/products/:storeId', require('./products'));
+router.get('/product-categories', require('./productCategories'));
 
 module.exports = router;
